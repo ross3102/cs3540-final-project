6158b1056da10e2a4af0eed1b9b7995562a31bfe
Ross Newman <rzn3102@gmail.com>
2024-02-16 00:42:06 -0500

    create enemy spawners and path following behavior

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject objective;
+    public Transform[] path;
+    public float spawnTime = 2; // seconds between each spawn
+    public int numEnemies = 5;
+    public bool spawnOnStart = true;
+
+    public GameObject enemyPrefab;
+
+    float timeSinceLastSpawn;
+
+    void Start()
+    {
+        if (objective == null) {
+            objective = GameObject.FindGameObjectWithTag("Objective");
+        }
+        if (spawnOnStart) {
+            SpawnEnemy();
+        }
+        timeSinceLastSpawn = 0;
+    }
+
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= spawnTime)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        if (numEnemies > 0)
+        {
+            var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            newEnemy.transform.SetParent(transform);
+            newEnemy.GetComponent<FollowPath>().SetPath(path, objective);
+            timeSinceLastSpawn -= spawnTime;
+            numEnemies--;
+        }
+    }
+}
---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public float speed = 1;
+    public int damage = 1;
+
+    Animator animator;
+
+    Transform[] path;
+    GameObject objective;
+
+    int currentObjectiveIdx = -1;
+    Vector3 currentObjective;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("Speed_f", 0.3f);
+    }
+
+    void Update()
+    {
+        if (currentObjectiveIdx >= 0) {
+            if (Vector3.Distance(transform.position, currentObjective) == 0) {
+                NextObjective();
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentObjective, Time.deltaTime * speed);
+        }
+    }
+
+    public void SetPath(Transform[] path, GameObject objective) {
+        this.path = path;
+        this.objective = objective;
+
+        currentObjectiveIdx = -1;
+
+        NextObjective();
+    }
+
+    void NextObjective() {
+        currentObjectiveIdx++;
+        
+        if (currentObjectiveIdx < path.Length) {
+            currentObjective = path[currentObjectiveIdx].position;
+        } else if (currentObjectiveIdx == path.Length) {
+            currentObjective = objective.transform.position;
+        } else {
+            objective.GetComponent<ObjectiveHealth>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+        transform.LookAt(currentObjective);
+    }
+}
