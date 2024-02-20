7d50c67e8f2bc1817d7ab9f4dfbfaac66ccfe051
Bob Zheng <bob.zheng6042@gmail.com>
2024-02-19 19:40:49 -0500

    added click damage plus animations

--- Assets/Scripts/ClickDamage.cs
+++ Assets/Scripts/ClickDamage.cs
@@ -5,37 +5,50 @@ using UnityEngine;
 public class ClickDamage : MonoBehaviour
 {
     public int damage;
-    public float hitRange = 5f;
+    public float hitRange = 10f;
 
     Animator animator;
+    bool canSwing;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        canSwing = true;
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) {
-            RaycastHit raycastHit;
-            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(transform.position, transform.forward, out raycastHit, hitRange))
+        if(Input.GetMouseButtonDown(0) && canSwing)
+        {
+            canSwing = false;
+            animator.SetFloat("Speed_f", 0f);
+            animator.SetInteger("WeaponType_int", 12);
+
+            Invoke("RegisterHit", 0.5f);
+            Invoke("StopSwinging", 1f);
+        }
+    }
+
+    private void RegisterHit()
+    {
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out raycastHit, hitRange))
+        {
+            if (raycastHit.transform != null)
             {
-                if (raycastHit.transform != null)
+                var clicked = raycastHit.transform.gameObject;
+                if (clicked.CompareTag("Enemy"))
                 {
-                   var clicked = raycastHit.transform.gameObject;
-                   if (clicked.CompareTag("Enemy")) {
-                       clicked.GetComponent<EnemyBehavior>().TakeDamage(damage);
-                   }
+                    clicked.GetComponent<EnemyBehavior>().TakeDamage(damage);
                 }
             }
-            animator.SetInteger("WeaponType_int", 12);
-            Invoke("StopSwinging", 1);
         }
     }
 
     void StopSwinging()
     {
         animator.SetInteger("WeaponType_int", 0);
+        canSwing = true;
     }
 }
