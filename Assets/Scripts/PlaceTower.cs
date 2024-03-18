8e6f90cf1e9e456d69fb3debc6f13efd205ac4ae
Bob Zheng <bob.zheng6042@gmail.com>
2024-03-17 21:53:56 -0400

    added grid system and towers can no longer be placed with overlapping colliders

--- Assets/Scripts/PlaceTower.cs
+++ Assets/Scripts/PlaceTower.cs
@@ -20,31 +20,32 @@ public class PlaceTower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        indicator.SetActive(true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
+        {
+            if (hit.collider.CompareTag("Floor"))
+            {
+                indicatorPos = ClosestGrid(hit.point);
+                indicator.transform.position = ClosestGrid(hit.point);
+            }
+            
+        }
+
         if (Input.GetButtonDown("Fire2"))
         {
-            indicator.SetActive(true);
-            RaycastHit hit;
-
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
+            if (hit.collider.CompareTag("Floor"))
             {
                 Instantiate(towerPrefab, indicatorPos + new Vector3(0, 2, 0), Quaternion.identity);
             }
         }
-        else
-        {
-            indicator.SetActive(true);
-            RaycastHit hit;
 
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
-            {
-                if (hit.collider.CompareTag("Floor"))
-                {
-                    indicatorPos = hit.point;
-                    indicator.transform.position = hit.point;
-                }
-            }
-        }
+    }
 
+    private Vector3 ClosestGrid (Vector3 coord)
+    {
+        return new Vector3(Mathf.Round(coord.x), Mathf.Round(coord.y), Mathf.Round(coord.z));
     }
 
     private void FixedUpdate()
