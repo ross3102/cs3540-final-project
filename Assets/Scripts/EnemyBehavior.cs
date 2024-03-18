c1ba877f6642157b01a17914b51148c614841370
meisbobzheng <94478583+meisbobzheng@users.noreply.github.com>
2024-03-18 12:47:02 -0400

    added sfx (#23)

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public int maxHealth = 10;
+    public AudioClip deathSFX;
 
     Animator animator;
     FollowPath followPath;
@@ -45,5 +46,6 @@ public class EnemyBehavior : MonoBehaviour
         GetComponent<Collider>().enabled = false;
         Destroy(gameObject, 2.5f);
         FindObjectOfType<LevelManager>().EnemyDestroyed();
+        AudioSource.PlayClipAtPoint(deathSFX, transform.position);
     }
 }
