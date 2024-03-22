43ef057f1ef65996402d8abcd5db2032a8794432
brady-br <91754190+brady-br@users.noreply.github.com>
2024-03-22 13:07:06 -0400

    Added money (#19)
    * Added money
    
    * Added starting cash and boundary walls
    
    * restore packages and project settings
    
    * move money to left
    
    ---------
    
    Co-authored-by: Ross Newman <rzn3102@gmail.com>

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public int maxHealth = 10;
+    public GameObject lootPrefab;
     public AudioClip deathSFX;
 
     Animator animator;
@@ -44,8 +45,17 @@ public class EnemyBehavior : MonoBehaviour
         animator.SetTrigger("Death_b");
         followPath.SetSpeed(0);
         GetComponent<Collider>().enabled = false;
-        Destroy(gameObject, 2.5f);
+        Invoke("DestroyEnemy", 2.5f);
         FindObjectOfType<LevelManager>().EnemyDestroyed();
         AudioSource.PlayClipAtPoint(deathSFX, transform.position);
+    }
+
+    void DestroyEnemy()
+    {
+        Instantiate(lootPrefab, transform.position, transform.rotation);
+
+        gameObject.SetActive(false);
+
+        Destroy(gameObject, 0.5f);
     }
 }
---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public int startingMoney = 10;
+    public Text moneyText;
+
+    int money;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = startingMoney;
+        SetMoneyText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetMoneyAmount()
+    {
+        return money;
+    }
+
+    public void AddMoney(int amount)
+    {
+        money += amount;
+        SetMoneyText();
+    }
+
+    public void SpendMoney(int amount)
+    {
+        money -= amount;
+        SetMoneyText();
+    }
+
+    public bool HasAtLeast(int amount)
+    {
+        return money >= amount;
+    }
+
+    void SetMoneyText()
+    {
+        moneyText.text = "$" + money.ToString();
+    }
+}
