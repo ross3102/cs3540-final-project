e77936967f14fe5de69ef78013255f53407638b3
Ross Newman <rzn3102@gmail.com>
2024-03-26 00:33:36 -0400

    improve tower placement

--- Assets/Scripts/PlaceTower.cs
+++ Assets/Scripts/PlaceTower.cs
@@ -6,32 +6,37 @@ public class PlaceTower : MonoBehaviour
 {
     public GameObject towerPrefab;
     public GameObject indicatorPrefab;
+    public GameObject radiusPreviewPrefab;
     public int towerCost = 10;
     public AudioClip placeSound;
+
+    public Color validColor, invalidColor;
 
     GameObject indicator;
     Vector3 indicatorPos;
     MoneyManager money;
+
+    bool indicatorValid;
     
-    // Start is called before the first frame update
     void Start()
     {
-        indicator = Instantiate(indicatorPrefab);
-        indicator.SetActive(false);
+        indicator = new GameObject();
+        var indicatorSquare = Instantiate(indicatorPrefab, indicator.transform.position, Quaternion.identity);
+        indicatorSquare.transform.SetParent(indicator.transform);
+        var towerDiameter = towerPrefab.GetComponent<ShootEnemies>().radius * 2;
+        radiusPreviewPrefab.transform.localScale = new Vector3(towerDiameter, 1, towerDiameter);
+        var radiusPreview = Instantiate(radiusPreviewPrefab, indicator.transform.position, Quaternion.identity);
+        radiusPreview.transform.SetParent(indicator.transform);
         money = GameObject.FindGameObjectWithTag("Player").GetComponent<MoneyManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (LevelManager.currentPhase != LevelManager.GamePhase.TowerPlacement) return;
 
         if (Input.GetButtonDown("Fire2") && money.HasAtLeast(towerCost))
         {
-            indicator.SetActive(true);
-            RaycastHit hit;
-
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
+            if (indicatorValid)
             {
                 Instantiate(towerPrefab, indicatorPos + new Vector3(0, 2, 0), Quaternion.identity);
                 money.SpendMoney(towerCost);
@@ -40,23 +45,30 @@ public class PlaceTower : MonoBehaviour
         }
         else
         {
-            indicator.SetActive(true);
-            RaycastHit hit;
+            var lookDirection = transform.forward;
+            var coef = transform.position.y / lookDirection.y;
+            var lookPoint = transform.position - lookDirection * coef;
 
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
+            indicatorPos = lookPoint;
+
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 50f))
             {
                 if (hit.collider.CompareTag("Floor"))
                 {
-                    indicatorPos = hit.point;
-                    indicator.transform.position = hit.point;
+                    indicatorValid = true;
                 }
+                else
+                {
+                    indicatorValid = false;
+                }
+            }
+            else
+            {
+                indicatorValid = false;
             }
         }
 
-    }
-
-    private void FixedUpdate()
-    {
-        
+        indicator.transform.position = indicatorPos;
+        indicator.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = indicatorValid ? Color.green : Color.red;
     }
 }
