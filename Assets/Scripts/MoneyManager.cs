4eb0971ff193ad991c73dd6c1badba3ff37d4052
Bob Zheng <bob.zheng6042@gmail.com>
2024-04-16 19:03:32 -0400

    money resets to level start amount when level lost

--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -8,12 +8,21 @@ public class MoneyManager : MonoBehaviour
     public int startingMoney = 10;
     public Text moneyText;
 
-    static int money = 0;
+    public static int levelStartMoney = 10;
+    public static bool restart = false;
+    public static int money = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        money += startingMoney;
+        if (restart)
+        {
+            money = levelStartMoney;
+        } else
+        {
+            money += startingMoney;
+            levelStartMoney = money;
+        }
         SetMoneyText();
     }
 
