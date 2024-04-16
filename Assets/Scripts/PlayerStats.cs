63c2e5901e56ee1483437ea7a384750cc9b7b3d9
Ross Newman <rzn3102@gmail.com>
2024-04-16 19:15:18 -0400

    remove playtime

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    float playtime;
     int attempts;
     int wins;
     int highScore;
@@ -13,14 +12,12 @@ public class PlayerStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playtime = PlayerPrefs.GetFloat("playtime", 0);
         attempts = PlayerPrefs.GetInt("attempts", 0);
         wins = PlayerPrefs.GetInt("wins", 0);
         highScore = PlayerPrefs.GetInt("highScore", 0);
 
-        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = playtime.ToString("hh':'mm':'ss") + "s";
-        transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = attempts.ToString();
-        transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = wins.ToString();
-        transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "$" + highScore;
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = attempts.ToString();
+        transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = wins.ToString();
+        transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "$" + highScore;
     }
 }
